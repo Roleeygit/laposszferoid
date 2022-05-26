0150959e5ca3c7fcc1adf501cde981822137cc9a
Roleeygit <juhaszroli2001@gmail.com>
2022-05-26 08:29:50 +0200

    Fejléc kész

---
+++ src/about.php
@@ -0,0 +1,9 @@
+<!--
+File: about.php
+Author: Juhász Roland
+Copyright: 2021, Juhász Roland
+Group: Szoft I N
+Date: 2022-06-26
+Github: https://github.com/Roleeygit/laposszferoid
+Licenc: GNU GPL
+-->
